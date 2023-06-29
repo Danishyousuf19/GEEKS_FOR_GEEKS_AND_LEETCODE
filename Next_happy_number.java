fa6300978967e6f5ebca0502bc7bd98fd1a48867
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-06-29 15:44:38 +0530

    Add files via upload

---
+++ Next_happy_number.java
@@ -0,0 +1,34 @@
+
+public class Next_happy_number {
+	static int nextHappy(int N){
+		int nxt=N+1;
+		while(true) {
+			if(chk(nxt)==1) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+	static int chk(int n) {
+		if(n==1||n==7)return 1;
+		int sum=n;
+		int current=n;
+		while(sum>9) {
+			sum=0;
+			while(current>0) {
+				int d=current%10;
+				sum+=d*d;
+				current/=10;
+				
+			}
+			sum=current;
+			if(sum==1||sum==7)return 1;
+		}
+		return 0;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
