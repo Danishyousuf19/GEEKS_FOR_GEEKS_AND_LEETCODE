4275f78be2b7d28ab7b8f3862475cba5820faf20
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-07-19 19:20:14 +0530

    Add files via upload

---
+++ longest_palendrom.java
@@ -0,0 +1,32 @@
+
+public class longest_palendrom {
+	 public static int longestPalinSubseq(String S)
+	    { String str1=S;
+	    String str2="";
+	    for (int i = 0; i < str1.length(); i++) {
+	        str2 = str1.charAt(i) + str2;
+	      }
+
+	         int[] L = new int[str1.length() + 1];
+
+	        for (int i = 1; i <= str2.length(); i++) {
+	            int prev = 0;
+	            for (int j = 1; j <= str1.length(); j++) {
+	                int current = L[j];
+	                 if (str1.charAt(j- 1) == str2.charAt(i- 1))
+	                    L[j] = prev + 1;
+	                else
+	                    L[j] = Math.max(L[j], L[j - 1]);
+	                prev = current;
+	            }
+	        }
+
+	        return L[str1.length()];
+	        //code here
+	    }
+	 public static void main(String[] args) {
+			// TODO Auto-generated method stub
+System.out.println(longestPalinSubseq("daninagf"));
+
+		}
+}
