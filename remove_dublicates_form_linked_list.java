b4e10b7429034efada115bed53535045bf54dcbf
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-07-22 11:36:02 +0530

    Add files via upload

--- remove_dublicates_form_linked_list.java
+++ remove_dublicates_form_linked_list.java
@@ -36,5 +36,28 @@ public class remove_dublicates_form_linked_list {
 		// TODO Auto-generated method stub
 
 	}
+	 public class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode() {}
+		      ListNode(int val) { this.val = val; }
+		      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+		  }
+		 
+		class Solutioncv {
+		    public ListNode deleteDuplicates(ListNode head) {
+		    	ListNode curr=head;
+		    	while(curr!=null&curr.next!=null) {
+		    		if(curr.val==curr.next.val) {
+		    			curr.next=curr.next.next;
+		    		}
+		    		else {
+		    			curr=curr.next;
+		    		}
+		    	}
+		        return head ;
+		        
+		    }
+		}
 
 }
