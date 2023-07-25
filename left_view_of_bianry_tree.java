6e30aca2cec3cbea0885922794d05532c46ff964
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-07-25 10:35:44 +0530

    Add files via upload

---
+++ left_view_of_bianry_tree.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class left_view_of_bianry_tree {
+	class Node
+	{
+	    int data;
+	    Node left, right;
+
+	    Node(int item)
+	    {
+	        data = item;
+	        left = right = null;
+	    }
+	}
+	 ArrayList<Integer> leftView(Node root)
+	    {
+	     
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
