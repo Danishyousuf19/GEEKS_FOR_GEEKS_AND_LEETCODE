d9832f9492dcf2ef4b77a5ee26df0066f673f082
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-08-06 13:42:12 +0530

    Add files via upload

--- spiral_tree.java
+++ spiral_tree.java
@@ -1,57 +1,97 @@
 import java.util.*;
-
 public class spiral_tree {
-	class Node
-	{
-	    int data;
-	    Node left, right;
-
-	    Node(int item)
-	    {
-	        data = item;
-	        left = right = null;
+	public class TreeNode {
+		    int val;
+		    TreeNode left;
+		    TreeNode right;
+		    TreeNode() {}
+		    TreeNode(int val) { this.val = val; }
+		    TreeNode(int val, TreeNode left, TreeNode right) {
+		        this.val = val;
+		        this.left = left;
+		        this.right = right;
+		    }
+		}
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+	    Queue<TreeNode> q = new LinkedList<>();
+	    List<List<Integer>> res = new ArrayList<>();
+	    int level = 1;
+	    
+	    if (root == null) {
+	        return res;
 	    }
+	    
+	    q.add(root);
+	    while (!q.isEmpty()) {
+	        int size = q.size();
+	        List<Integer> lev = new ArrayList<>();
+	        
+	        for (int i = 0; i < size; i++) {
+	            TreeNode curr = q.poll();
+	            if (level % 2 == 0) {
+	                lev.add(0, curr.val);  // Add to the beginning of the list
+	            } else {
+	                lev.add(curr.val);
+	            }
+	            if (curr.left != null) {
+	                q.add(curr.left);
+	            }
+	            if (curr.right != null) {
+	                q.add(curr.right);
+	            }
+	        }
+	        
+	        res.add(lev);
+	        level++;
+	    }
+	    
+	    return res;
 	}
-        // code here 
-	
-	ArrayList<Integer> findSpiral(Node root) 
-    {
-        ArrayList<Integer>result=new ArrayList<>();
-        Queue<Node>q=new LinkedList<>();
-        Stack<Integer>s=new Stack<>();
-        if(root==null) return result;
-        q.add(root);
-        
-        int level=1;
-        
-        while(!q.isEmpty()) {
-        	int size = q.size();
-        	for(int i=0;i<size;i++) {
-        		Node curr=q.poll();
-        		if(level%2==0)
-        			result.add(curr.data);
-        		else 
-        			s.push(curr.data);
-        		if(curr.left!=null)
-        			q.add(curr.left);
-        		if(curr.right!=null)
-        			q.add(curr.right);
-        	}
-        	if(level%2!=0) {
-        		while(!s.isEmpty()) {
-        			result.add(s.pop());
-        		}
-        	}
-        	level++;
-        }
-        
-        
-        return result;
-    }
-    
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	public List<List<Integer>> zigzagLqevelOrder(TreeNode root) {
+	    Queue<TreeNode> q = new LinkedList<>();
+	    List<List<Integer>> res = new ArrayList<>();
+	    Stack<Integer> s = new Stack<>();
+	    int level = 1;
+	    
+	    if (root == null) {
+	        return res;
+	    }
+	    
+	    q.add(root);
+	    while (!q.isEmpty()) {
+	        int size = q.size();
+	        List<Integer> lev = new ArrayList<>();
+	        
+	        for (int i = 0; i < size; i++) {
+	            TreeNode curr = q.poll();
+	            if (level % 2 == 0) {
+	                s.push(curr.val);  // Use the stack for even levels
+	            } else {
+	                lev.add(curr.val);
+	            }
+	            if (curr.left != null) {
+	                q.add(curr.left);
+	            }
+	            if (curr.right != null) {
+	                q.add(curr.right);
+	            }
+	        }
+	        
+	        while (!s.isEmpty()) {
+	            lev.add(s.pop());  // Pop values from the stack for odd levels
+	        }
+	        
+	        res.add(lev);
+	        level++;
+	    }
+	    
+	    return res;
+	}
+
 
 }
