8f4211938282610df18f559bcccf2dc9aa97b197
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-09-11 12:44:38 +0530

    Add files via upload

---
+++ uglyNo.java
@@ -0,0 +1,13 @@
+
+public class uglyNo {
+
+	  public boolean isUgly(int n) {
+		  if(n<=0)return false;
+	        for(int i=2;i<6&&n>0;i++) {
+	        	while(n%i==0) {
+	        		n/=i;
+	        	}
+	        }
+	        return n==1;
+	    }
+}
