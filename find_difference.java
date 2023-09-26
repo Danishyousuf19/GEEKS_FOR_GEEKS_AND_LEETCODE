a6461c7954f01e977d7277dc814b1e56ebf30275
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-09-26 08:32:31 +0530

    Delete find_difference.java

--- find_difference.java
+++
@@ -1,9 +0,0 @@
-
-public class find_difference {
-	 public char findTheDifference(String s, String t) {
-	        char c=0;
-	        for(char sc:s.toCharArray()) c^=sc;
-	        for(char tc:t.toCharArray())c^=tc;
-	        return c;
-	    }
-}
