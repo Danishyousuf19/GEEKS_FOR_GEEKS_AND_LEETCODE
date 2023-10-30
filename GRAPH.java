e7896e561ad0290905a2c5ff5e3c4f3a29afbcfc
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-10-30 23:03:16 +0530

    Add files via upload

--- GRAPH.java
+++ GRAPH.java
@@ -23,6 +23,42 @@ public class GRAPH {
 		graph[3].add(new edge(3,1));
 		graph[3].add(new edge(3,2));
 	}
+static void bfs(ArrayList<edge>graph[],int v) {
+	Queue<Integer>q=new LinkedList();
+	boolean vis[]=new boolean[v];
+	q.add(0);
+	while(!q.isEmpty()) {
+		int curr=q.remove();
+		if(vis[curr]==false) {
+			System.out.print(curr+" ");
+			vis[curr]=true;
+			for(int  i=0;i<graph[curr].size();i++) {
+				edge e=graph[curr].get(i);
+				q.add(e.dest);
+			}
+			
+		}
+	}
+	
+}
+static void bfs2(ArrayList<edge>graph[],int cur ,boolean[]vis) {
+	Queue<Integer>q=new LinkedList();
+	
+	q.add(cur);
+	while(!q.isEmpty()) {
+		int curr=q.remove();
+		if(vis[curr]==false) {
+			System.out.print(curr+" ");
+			vis[curr]=true;
+			for(int  i=0;i<graph[curr].size();i++) {
+				edge e=graph[curr].get(i);
+				q.add(e.dest);
+			}
+			
+		}
+	}
+	
+}
 	public static void main(String[] args) {
 		int  v=4;//vertics no
 		ArrayList<edge>graph[]=new ArrayList[v];//Initially null//first change to empty
@@ -31,6 +67,14 @@ public class GRAPH {
 		for(int i=0;i<graph[2].size();i++) {
 			edge e=graph[2].get(i);
 			System.out.println(e.src+"-->"+e.dest);
+		}
+		bfs(graph, v);
+//for not connneccted
+		for(int i=0;i<v;i++) {
+			boolean vis[]=new boolean[v];
+			if(vis[i]==false) {
+				bfs2(graph, i, vis);
+			}
 		}
 	}
 }
