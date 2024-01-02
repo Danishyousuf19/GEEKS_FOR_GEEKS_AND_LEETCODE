ddbe38b2fa36941489582d64e5f7d477aefb0df7
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2024-01-02 11:03:35 +0530

    Update convert1d_to_2d.java

--- convert1d_to_2d.java
+++ convert1d_to_2d.java
@@ -26,4 +26,20 @@ fm.put(i, fm.getOrDefault(i, 0) + 1);
      }    
      return ans;
     }
+	  public List<List<Integer>> finhdMatrix(int[] nums) {
+        int freq[] = new int[nums.length + 1];
+
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        for (int c : nums) {
+            if (freq[c] >= ans.size()) {
+                ans.add(new ArrayList<>());
+            }
+
+            // Store the integer in the list corresponding to its current frequency.
+            ans.get(freq[c]).add(c);
+            freq[c]++;
+        }
+
+        return ans;
+    }
 }
