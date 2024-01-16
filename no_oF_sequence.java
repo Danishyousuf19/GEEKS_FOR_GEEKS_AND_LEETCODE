bd2758eac1ad042fe11adab8bbd0d96c142bf291
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2024-01-16 10:12:40 +0530

    Add files via upload

---
+++ no_oF_sequence.java
@@ -0,0 +1,17 @@
+
+public class no_oF_sequence {
+
+	static int numberSequence(int m, int n) {
+		int[][] dp = new int[n + 1][m + 1];
+
+		for (int i = 0; i <= m; i++)
+			dp[0][i] = 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				dp[i][j] = dp[i - 1][j / 2] + dp[i][j - 1];
+			}
+		}
+		return dp[n][m];
+	}
+
+}
