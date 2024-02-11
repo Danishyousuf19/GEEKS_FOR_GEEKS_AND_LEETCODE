6671af63c34a281b6c070411d0268deaf196c8c6
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2024-02-11 10:35:52 +0530

    Add files via upload

---
+++ rsSeries.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class rsSeries {
+	  static ArrayList<Integer> recamanSequence(int n){
+	        // code here
+	        ArrayList<Integer>list=new ArrayList<>();
+	        list.add(0);
+	        HashSet<Integer>set=new HashSet<>();
+	        set.add(0);
+	        for(int i=1;i<=n;i++){
+	            int val=list.get(i-1)-i;
+	            if(val<0||set.contains(val)){
+	                val=list.get(i-1)+i;
+	            }
+	            list.add(val);
+	            set.add(val);
+	        }
+	        return list;
+	    }
+}
