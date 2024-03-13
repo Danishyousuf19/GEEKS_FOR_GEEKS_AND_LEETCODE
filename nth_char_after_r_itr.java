9ddd2f3504218db22c50ebad78554d5bb9b87e56
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2024-03-13 11:32:00 +0530

    Add files via upload

---
+++ nth_char_after_r_itr.java
@@ -0,0 +1,20 @@
+
+public class nth_char_after_r_itr {
+	 public char nthCharacter(String s, int r, int n)
+	    {
+	        //code here
+	        while(r-- > 0) {
+	            String m = "";
+	            for(int i = 0; i < Math.min(s.length(),n+1); i++) {
+	                if(s.charAt(i) == '1') {
+	                    m+="10";
+	                }
+	                else {
+	                    m+="01";
+	                }
+	            }
+	            s = m;
+	        }
+	        return s.charAt(n);
+	    }
+}
