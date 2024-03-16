cb069d07e2c65e705d8f31bd64527d42a7423192
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2024-03-16 10:17:43 +0530

    Add files via upload

---
+++ sort_linked_del_node.java
@@ -0,0 +1,36 @@
+
+public class sort_linked_del_node {
+	class Node{
+		int data;
+		Node next;
+	}
+
+
+    
+
+	public Node sort(Node head){
+        Node n1 = head;
+        Node n2 = head.next;
+        while(n2!=null){
+            if(n1.data>n2.data){
+                int t=n1.data;
+                n1.data=n2.data;
+                n2.data=t;
+            } 
+                n2=n2.next;
+        if(n2==null){
+                n1=n1.next;
+                n2=n1.next;
+            }
+        }
+        return head;
+        }
+	   void deleteNode(Node del_node)
+	    {
+	         // Your code here
+	         del_node.data = del_node.next.data;
+	        del_node.next = del_node.next.next;
+	    }
+
+ 
+}
